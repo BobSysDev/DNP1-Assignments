70b2a02d06c1d7f639b79859adc9d26a2b649325
Electimore <electimore@gmail.com>
2024-09-24 10:34:35 +0200

    FileRepository for Comments

--- CLI/Program.cs
+++ CLI/Program.cs
@@ -1,10 +1,11 @@
 ﻿using CLI.UI;
+using Filerepositories;
 using InMemoryRepositories;
 using RepositoryContracts;
 
 Console.WriteLine("Starting CLI app...");
 IUserRepository userRepository = new UserInMemoryRepository();
-ICommentRepository commentRepository = new CommentInMemoryRepository();
+ICommentRepository commentRepository = new CommentFileRepository();
 IPostRepository postRepository = new PostInMemoryRepository();
 
 CliApp cliApp = new CliApp(userRepository, commentRepository, postRepository);
