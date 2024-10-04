ceb30894e7de3b7fb4f5a45268b78109e1179b3f
Marius Gafton <mariusgafton2005@gmail.com>
2024-10-04 11:32:44 +0200

    PostDTO and controller

--- Server/WebAPI/Controllers/PostsController.cs
+++ Server/WebAPI/Controllers/PostsController.cs
@@ -1,4 +1,6 @@
 ﻿using DTOs;
+using Entities;
+using Microsoft.AspNetCore.Http.HttpResults;
 using Microsoft.AspNetCore.Mvc;
 using RepositoryContracts;
 
@@ -15,9 +17,58 @@ public class PostsController
       _postRepository = postRepository;
    }
 
-   [HttpGet]
-   public async Task<ActionResult<IEnumerable<PostDTO>>> GetUsers([FromQuery])
+   [HttpPost]
+   public async Task<IResult> CreateNewPost([FromBody] PostDTO postDto)
    {
+     if (string.IsNullOrEmpty(postDto.Title))
+     {
+        return Results.BadRequest("Post title is required");
+     }
+
+     Post newPost = new Post()
+     {
+        Title = postDto.Title,
+        Body = postDto.Body,
+        UserId = postDto.UserId
+     };
+     
+     Post createdPost = await _postRepository.AddAsync(newPost);
+     
+     return Results.Created($"posts/{createdPost.PostId}", createdPost);
+   }
+   
+   [HttpGet("{id}")]    
+
+   public async Task<IResult> GetSinglePost([FromRoute] string id)
+   {
+      try
+      {
+         Post post = await _postRepository.GetSingleAsync(id);
+         return Results.Ok(post);
+      }
+      catch (Exception e)
+      {
+         Console.WriteLine(e);
+         return Results.NotFound(e.Message);
+      }
+   }
+   
+   [HttpGet]
+   
+   public async Task<IResult> GetMany([FromQuery] int? userId, [FromQuery] string? title)
+   {
+      IQueryable<Post> posts = _postRepository.GetMany();
       
+      if (userId is not null)
+      {
+         posts = posts.Where(p => p.UserId == userId);
+      }
+      
+      if (title is not null)
+      {
+         posts = posts.Where(p => p.Title.Contains(title));
+      }
+      
+      return Results.Ok(posts);
    }
 }
--- Shared/DTOs/CommentDTO.cs
+++ Shared/DTOs/CommentDTO.cs
@@ -2,9 +2,9 @@
 
 public class CommentDTO
 {
-    public string Id { get; set; }
+    public int Id { get; set; }
     public string Body { get; set; }
-    public string UserId { get; set; }
+    public int UserId { get; set; }
     public string ParentId { get; set; }
     public List<CommentDTO> Children { get; set; }
 }
