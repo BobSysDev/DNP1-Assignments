2af747ef4f96c35078fab2897f3344355d1b4d3f
KVECKO <samo.knieza@gmail.com>
2024-10-22 16:26:04 +0200

    usercontroller not done

--- Server/WebAPI/Controllers/UsersController.cs
+++ Server/WebAPI/Controllers/UsersController.cs
@@ -16,11 +16,15 @@ public class UsersController : ControllerBase
     {
         this.userRepo = userRepo;
     }
-    
-    
-    [HttpPost] public async Task<ActionResult<User>> AddUser([FromBody] UserDTO request) { await VerifyUserNameIsAvailableAsync(request.Username, request.Id);
 
-        User user = new(request.Username, request.Password, request.Id); User created = await userRepo.AddAsync(user);
+
+    [HttpPost]
+    public async Task<ActionResult<User>> AddUser([FromBody] UserDTO request)
+    {
+        //await VerifyUserNameIsAvailableAsync(request.Username, request.Id);
+
+        User user = new(request.Username, request.Password, request.Id);
+        User created = await userRepo.AddAsync(user);
 
         UserDTO dto = new()
         {
@@ -30,31 +34,67 @@ public class UsersController : ControllerBase
             Username = created.Username
 
         };
-        return Created($"/Users/{dto.Id}", created);}
-
-    
-    private async Task VerifyUserNameIsAvailableAsync(string newUsername, int userId)
-    {
-        var currentUser = await userRepo.GetSingleAsync(userId);
-    
-        if (currentUser == null)
-        {
-            throw new InvalidOperationException("User not found.");
-        }
-
-        if (currentUser.Username == newUsername)
-        {
-            return;
-        }
-
-        var existingUser = await userRepo.GetByUsernameAsync(newUsername);
-        if (existingUser != null)
-        {
-            throw new InvalidOperationException("Username is already taken.");
-        }
+        return Created($"/Users/{dto.Id}", created);
     }
 
-    
-    
-    
+
+    private async Task VerifyUserNameIsAvailableAsync(string newUsername, int userId)
+    {
+        // var currentUser = await userRepo.GetSingleAsync(userId);
+        //
+        // if (currentUser == null)
+        // {
+        //     throw new InvalidOperationException("User not found.");
+        // }
+        //
+        // if (currentUser.Username == newUsername)
+        // {
+        //     return;
+        // }
+        //
+        // var existingUser = await userRepo.GetByUsernameAsync(newUsername);
+        // if (existingUser != null)
+        // {
+        //     throw new InvalidOperationException("Username is already taken.");
+        // }
+    }
+
+    [HttpPatch]
+    public async Task<ActionResult<User>> UpdateUser([FromBody] UserDTO request)
+    {
+        User user = new(request.Username, request.Password, request.Id);
+        userRepo.UpdateAsync(user);
+        User updated = await userRepo.GetSingleAsync(user.Id);
+        UserDTO dto = new()
+        {
+            Id = updated.Id,
+            Username = updated.Username
+        };
+        return Accepted($"/Users/{dto.Id}", updated);
+    }
+
+    [HttpGet]
+    public async Task<ActionResult<User>> GetSingle([FromBody] UserDTO request)
+    {
+        User gotUser = await userRepo.GetSingleAsync(request.Id);
+        if (gotUser is null)
+        {
+            throw new InvalidOperationException("User does not exist.");
+        }
+
+        UserDTO dto = new()
+        {
+            Id = gotUser.Id,
+            Username = gotUser.Username
+        };
+        return Accepted($"/Users/{dto.Id}", gotUser);
+    }
+
+    [HttpGet]
+    public async Task<ActionResult<List<User>>> GetMany([FromBody] string username)
+    {
+        List<User> users = new List<User>();
+        users.AddRange(userRepo.GetMany());
+        users.Where(user => usersFound.Contains(user.Username));    
+    }
 }
